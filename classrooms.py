1b34575503842e2ab752a2b08e4e16f6ca1c6f6e
Parker Henry <parkerhenry0@gmail.com>
2015-01-17 04:02:56 -0500

    Code gets classes that happen at the regular intervals of day specified, NOT necessarily classes that actually happen on said day.

---
+++ classrooms.py
@@ -0,0 +1,41 @@
+from penn.registrar import Registrar
+from credentials import REG_USERNAME, REG_PASSWORD
+
+r = Registrar(REG_USERNAME, REG_PASSWORD)
+day_param = 'starts_on_day'
+start_param = 'starts_at_or_after_hour'
+end_param = 'ends_at_or_after_hour'
+
+days_of_the_week = "MTWRFSU"
+
+def get_used_rooms(start_time, end_time, day):
+    sections_generators = [get_sections_starting_on_day(start_time, end_time, d)
+            for d in days_to_search(day)]
+    """sections = sections_generators[0]
+    return [[i['meetings'][0]['building_code'], 
+             i['meetings'][0]['room_number']]
+            for i in sections ]
+"""
+    list_of_room_lists = [[[i['meetings'][0]['building_code'],
+                            i['meetings'][0]['room_number']]
+                           for i in generator] for generator in
+                           sections_generators]
+    return list_of_room_lists
+
+def get_sections_starting_on_day(start_time, end_time, day):
+    sections = r.search({
+        day_param : day,
+        start_param : start_time,
+        end_param : end_time,
+        'course_id' : 'phys'
+        })
+    return sections
+
+def days_to_search(day):
+    index = days_of_the_week.index(day)
+    days = []
+    while(index >= 0):
+        days.append(days_of_the_week[index])
+        index -= 2
+    return days
+
